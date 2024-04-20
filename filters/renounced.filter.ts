210f415cdbaec5204ee9dfe942a6e7462c9f64ad
pgpetrov2001 <pgpetrov2001@gmail.com>
2024-04-20 16:46:41 +0300

    ran prettier with new config

--- filters/renounced.filter.ts
+++ filters/renounced.filter.ts
@@ -5,22 +5,22 @@ import { LiquidityPoolKeysV4 } from '@raydium-io/raydium-sdk';
 import { logger } from '../helpers';
 
 export class RenouncedFilter implements Filter {
-  constructor(private readonly connection: Connection) {}
+	constructor(private readonly connection: Connection) {}
 
-  async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
-    try {
-      const accountInfo = await this.connection.getAccountInfo(poolKeys.baseMint, this.connection.commitment);
-      if (!accountInfo?.data) {
-        return { ok: false, message: 'Renounced -> Failed to fetch account data' };
-      }
+	async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
+		try {
+			const accountInfo = await this.connection.getAccountInfo(poolKeys.baseMint, this.connection.commitment);
+			if (!accountInfo?.data) {
+				return { ok: false, message: 'Renounced -> Failed to fetch account data' };
+			}
 
-      const deserialize = MintLayout.decode(accountInfo.data);
-      const renounced = deserialize.mintAuthorityOption === 0;
-      return { ok: renounced, message: renounced ? undefined : 'Renounced -> Creator can mint more tokens' };
-    } catch (e) {
-      logger.error({ mint: poolKeys.baseMint }, `Failed to check if mint is renounced`);
-    }
+			const deserialize = MintLayout.decode(accountInfo.data);
+			const renounced = deserialize.mintAuthorityOption === 0;
+			return { ok: renounced, message: renounced ? undefined : 'Renounced -> Creator can mint more tokens' };
+		} catch (e) {
+			logger.error({ mint: poolKeys.baseMint }, `Failed to check if mint is renounced`);
+		}
 
-    return { ok: false, message: 'Renounced -> Failed to check if mint is renounced' };
-  }
+		return { ok: false, message: 'Renounced -> Failed to check if mint is renounced' };
+	}
 }
