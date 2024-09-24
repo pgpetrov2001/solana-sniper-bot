db85c6833049bd5f29fe7f14f6c1711628240198
pgpetrov2001 <pgpetrov2001@gmail.com>
2024-09-24 21:15:09 +0300

    added new utility function for computing set difference, and updated range utlity function to use older JS

--- helpers/common.ts
+++ helpers/common.ts
@@ -1,6 +1,6 @@
 export const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export const range = (n: number) => [...Array(n).keys()];
+export const range = (n: number) => new Array(n).fill(0).map((_: number, i: number) => i);
 
 //as per: https://stackoverflow.com/questions/62116454/how-to-type-define-a-zip-function-in-typescript
 export function zip<T extends unknown[][]>(
@@ -9,6 +9,16 @@ export function zip<T extends unknown[][]>(
 	const minLength = Math.min(...args.map((arr) => arr.length));
 	// @ts-expect-error This is too much for ts
 	return range(minLength).map((i) => args.map((arr) => arr[i]));
+}
+
+export function setDifference<T>(A: Array<T>, B: Array<T>): Array<T> {
+	const setA = new Set(A);
+	const setB = new Set(B);
+	const difference = new Set<T>(setA);
+	for (const elem of setB) {
+		difference.delete(elem);
+	}
+	return Array.from(difference);
 }
 
 export class Deferred {
